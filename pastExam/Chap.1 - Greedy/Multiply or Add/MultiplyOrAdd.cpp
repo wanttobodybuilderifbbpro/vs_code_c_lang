970a2868fe00e49cee60e4885e62d4f810e51432
wanttobodybuilderifbbpro <wogud114@gmail.com>
2023-11-27 22:17:51 +0900

    231127

---
+++ pastExam/Chap.1 - Greedy/Multiply or Add/MultiplyOrAdd.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+string str;
+
+int main(){
+  cin >> str;
+  
+  //첫 번째 문자를 숫자로 변경한 값을 대입
+  long long result = str[0] - '0';
+
+  for(int i = 1; i < str.size(); i++){
+    //두 수 중에서 하나라도 0 혹은 1일 경우에는 × 보다는 ＋
+    int num = str[i] - '0';
+    if(num <= 1 || result <= 1){
+      result += num;
+    }
+    else {
+      result *= num;
+    }
+  }
+
+  cout << result << '\n';
+}
