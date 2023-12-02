9ef6fa174b25462e19bcda40dc66f2fdded2a10c
wanttobodybuilderifbbpro <wogud114@gmail.com>
2023-12-03 02:01:14 +0900

    231203

---
+++ pastExam/Chap.1 - Greedy/SelectionOfTenPinBowling/SelectionOfTenPinBowling.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m, k;
+vector <int> arr;
+
+int main(){
+  cin >> k;
+
+  for(int i = 0; i < k; i++){
+    int x;
+    cin >> x;
+    arr.push_back(x);
+  }
+
+  //입력하다가 같은게 나오면 break 걸고 그렇지 않으면 continue
+  for(int i = 0; i < arr.length; i++){
+    if(){
+
+    }
+    else break;
+  }
+
+  cout << << '\n';
+}
